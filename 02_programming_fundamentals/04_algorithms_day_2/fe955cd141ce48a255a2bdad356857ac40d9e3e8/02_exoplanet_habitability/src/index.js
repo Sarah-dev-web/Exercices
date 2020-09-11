dd71cf0f16191438f3196cf1097228eb16643ec9
Sarah <sarah.hammadi808@gmail.com>
2020-09-11 16:57:22 +0200

    exo2 exoplan

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/02_exoplanet_habitability/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/02_exoplanet_habitability/src/index.js
@@ -8,8 +8,19 @@ const planet = {
 };
 
 function canHabitateLife(star, planet) {
-  // Code the function here.
-}
+  if((star.spectralClass = "M" || "K") && (planet.mass > 0.32) && (0.5<planet.radius< 2.5) && (planet.rotationStability === true)){
+    return true;
 
+  }else{
+    return false;
+  }
+}
+console.log(canHabitateLife(star,planet));
 // Do not remove last lines, it is for tests
 module.exports = canHabitateLife;
+
+//The radius of a potentially habitable exoplanet would range between 0.5 and 2.5 earth radii. 
+//A mineral planet would need at least a mass starting from 0.32 earth masses to be suitableÒ
+
+//Athough, if a planet’s mass is smaller than 0.5 earth masses, it’s radius would need to be larger than 1 earth radii to maintain life.  
+//A stable rotation is a status we give planets that have a fast rotation that doesn't go through large changes over time.
