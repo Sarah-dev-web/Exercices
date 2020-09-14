7a5e5fa887389569d950b0bafe2faf54429a639a
Sarah <sarah.hammadi808@gmail.com>
2020-09-14 13:16:06 +0200

    callback1 done

---
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/01_create_callback/src/callback.js
@@ -0,0 +1,8 @@
+// code here
+const sayHello = (work) => {
+  const player = { name: "Spartacus" };
+  const name = work.name;
+  console.log(`Hello, ${name}`);
+};
+// leave the code below for tests to work
+module.exports = sayHello;
