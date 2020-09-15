a322f5b1969efda7bd996db28bec6e6cfcbc5922
Sarah <sarah.hammadi808@gmail.com>
2020-09-15 12:02:02 +0200

    First program complete

---
+++ 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/01_identity_form/src/identityForm.js
@@ -0,0 +1,20 @@
+const identityForm = (reader) => {
+  // Code here
+  reader.question("What is your first name? ", (FirstName) => {
+    
+     
+    reader.question("What is your last name? ", (lastName) => {
+      
+    
+      reader.question("What is your age? ", (age) => {
+        console.log(`Your name is ${FirstName} ${lastName} and you are ${age}.`);
+        reader.close();
+      });
+    });
+  });
+};
+
+
+
+// Leave line below for tests to work
+module.exports = identityForm;
