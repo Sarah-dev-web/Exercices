19d8bb9d32fc663de7aa3bf7c23fcdd6a5aaab8f
Sarah <sarah.hammadi808@gmail.com>
2020-09-15 09:42:59 +0200

    récup exo firstpro

---
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/02_use_callback/src/getInput.js
@@ -0,0 +1,10 @@
+// A function that takes two parameters, the second one being a callback function
+
+const getInput = ({user : name, key : the_pressed_key},callback) => {
+  const user = "Toto";
+  const key = "A";
+  callback();
+};// code the function here
+
+// Leave the line below for tests to work
+module.exports = getInput;
