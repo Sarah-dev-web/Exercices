8904a3fb8840498866ba5d05137d3fb29dd012b6
Sarah <sarah.hammadi808@gmail.com>
2020-09-16 18:00:15 +0200

    coffee is ready

---
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/03_coffee_machine/src/coffeeMachine.js
@@ -0,0 +1,27 @@
+const machine = {
+  // Complete here
+  litersOfCoffee: 0,
+  fillWithLitersOfCoffee: function(litersOfCoffee){
+    this.litersOfCoffee += litersOfCoffee;
+  },
+  expresso : function(){
+    if(this.litersOfCoffee >= 0.08){
+      this.litersOfCoffee = this.litersOfCoffee - 0.08;
+      return true;
+    }else{
+      return false;
+    }
+  },
+  longCoffee : function (){
+    if(this.litersOfCoffee >= 0.15){
+      this.litersOfCoffee = this.litersOfCoffee - 0.15;
+      return true;
+    }else{
+      return false;
+    }
+  },
+  
+};
+
+
+module.exports = machine;
