7070d995bb098614da1cd815c34e6cefb7fc9bca
Sarah <sarah.hammadi808@gmail.com>
2020-09-20 18:56:17 +0200

    fizzbuzz done

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/03_fizz_buzz/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/03_fizz_buzz/src/index.js
@@ -1,6 +1,18 @@
-function fizzBuzz() {
-  
+function fizzBuzz(list) {
+  const returnedArray = [];
+
+  for (let index = 0; index < list.length; index++) {
+    if (list[index] % 3 === 0 && list[index] % 5 === 0) {
+      returnedArray.push("FizzBuzz");
+    } else if (list[index] % 3 === 0) {
+      returnedArray.push("Fizz");
+    } else if (list[index] % 5 === 0) {
+      returnedArray.push("Buzz");
+    } else {
+      returnedArray.push(list[index]);
+    }
+  }
+  return returnedArray;
 }
 
-// Leave the line below untouched for tests to work properly.
 module.exports = fizzBuzz;
