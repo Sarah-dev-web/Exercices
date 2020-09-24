257f7b0d8a27405d94290148984323f64deefac3
Sarah <sarah.hammadi808@gmail.com>
2020-09-24 12:53:51 +0200

    Récup exo

---
+++ 03_backend_developement/01_typescript_101/1bc25b477fadbef8f726030942699167117a51ef/03_typed_class/src/index.ts
@@ -0,0 +1,22 @@
+class Bird {
+  // Write your code here.
+  // Properties member.
+  age: number;
+
+  // Constructor member.
+
+  constructor( Givenage : number,) {
+    this.age = Givenage;
+    //methods
+    sing () : string {
+      console.log("Cui cui");
+    }
+    fly () : {
+      if()
+    }
+    
+  }
+}
+
+// Leave the line below for tests to work properly.
+export { Bird };
