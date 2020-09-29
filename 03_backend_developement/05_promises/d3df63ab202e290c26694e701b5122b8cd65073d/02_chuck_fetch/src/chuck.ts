4c67c60d0aab31051cfaac6e75475ddc8e83886a
Sarah <sarah.hammadi808@gmail.com>
2020-09-29 15:47:01 +0200

    chuck fetch done

---
+++ 03_backend_developement/05_promises/d3df63ab202e290c26694e701b5122b8cd65073d/02_chuck_fetch/src/chuck.ts
@@ -0,0 +1,16 @@
+import fetch, { Response } from "node-fetch";
+
+function getCategories(): Promise<string[]> {
+  // Your code goes here
+  return fetch(`https://api.chucknorris.io/jokes/categories`).then((response) => response.json());
+}
+
+function getChuckNorrisJoke(category: string): Promise<string> {
+  // Your code goes here
+  return fetch(`https://api.chucknorris.io/jokes/random?category=${category}`).then((response) =>
+    response.json().then((joke) => joke.value),
+  );
+}
+
+// Leave the line below for tests to work properly
+export { getCategories, getChuckNorrisJoke };
