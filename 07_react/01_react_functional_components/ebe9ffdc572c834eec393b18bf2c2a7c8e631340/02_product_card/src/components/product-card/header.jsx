8845e79b28b04a65c1c10e44a406ba041f283325
Sarah <sarah.hammadi808@gmail.com>
2020-11-05 09:40:19 +0100

    exo en cours

--- 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/02_product_card/src/components/product-card/header.jsx
+++ 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/02_product_card/src/components/product-card/header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const CardHeader = () => {
-  return <div />;
+const CardHeader = ({name, platformLogos}) => {
+  return (
+  <div className="bg-dark text-white p-3 card-header">
+    <p>{name}</p>
+    <div className></div>
+  </div> 
+  );
 };
 
 export default CardHeader;
--- 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/02_product_card/src/components/product-card/index.jsx
+++ 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/02_product_card/src/components/product-card/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import CardHeader from "./header";
+import CardBody from "./body";
+import CardFooter from "./footer";
 
-const ProductCard = () => {
-  return <div />;
+
+const ProductCard = ({product}) => {
+
+  const platformLogos = product.platforms.map(
+    (platform) => platform.platform_logo.url,
+  );
+  return (
+  <div className="border">
+    <CardHeader name = {product.name}
+    platformLogos = {platformLogos}>
+    </CardHeader>
+    <CardBody></CardBody>
+    <CardFooter></CardFooter>
+  </div> 
+  );
 };
 
 export default ProductCard;
